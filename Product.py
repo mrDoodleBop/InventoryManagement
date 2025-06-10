c6f907878d0cd9ed043787bcbd911bdf25151c93
Michael Cates <73615515+mrDoodleBop@users.noreply.github.com>
2025-06-09 21:39:14 -0500

    Added initial project files
    This includes the Product class file and the ProductManager class file which will be the heart of the organizer. This is how the information for each product entry will be organized for ease of use and manipulation throughout the running of the program.

---
+++ Product.py
@@ -0,0 +1,6 @@
+'''
+File Name : Product.py
+Author Name : Michael Cates
+Date : 6/9/2025
+Purpose :
+'''
---
+++ ProductManager.py
@@ -0,0 +1,6 @@
+'''
+File Name : ProductManager.py
+Author Name : Michael Cates
+Date : 6/9/2025
+Purpose :
+'''
